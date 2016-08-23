f4056b9b801558b07d04021fe176401580933022
Lim Jisu <lim_js91@naver.com>
2016-08-23 12:48:43 +0900

    fragment

---
+++ app/src/main/java/com/tacademy/singleplay/OperaFragment.java
@@ -0,0 +1,29 @@
+package com.tacademy.singleplay;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OperaFragment extends Fragment {
+
+
+    public OperaFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_opera, container, false);
+    }
+
+}
