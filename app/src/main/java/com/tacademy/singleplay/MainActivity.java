f3f6cf7bee0603571df961053997fdc083b4290e
Lim Jisu <lim_js91@naver.com>
2016-08-23 16:46:37 +0900

    4:46

--- app/src/main/java/com/tacademy/singleplay/MainActivity.java
+++ app/src/main/java/com/tacademy/singleplay/MainActivity.java
@@ -1,22 +1,39 @@
 package com.tacademy.singleplay;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.tacademy.singleplay.detail.UserActivity;
 import com.tacademy.singleplay.main.ConcertFragment;
 import com.tacademy.singleplay.main.MDRecFragment;
 import com.tacademy.singleplay.main.MusicalFragment;
 import com.tacademy.singleplay.main.OperaFragment;
 import com.tacademy.singleplay.main.WishListFragment;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tabHost)
     FragmentTabHost tabHost;
+    @BindView(R.id.text_toolbarTitle)
+    TextView toolbarTitleView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tab_linear)
+    LinearLayout tabLayout;
 
     private static final String TAB_MUSICAL = "tab musical";
     private static final String TAB_OPERA = "tab opera";
@@ -29,31 +46,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        toolbarTitleView.setText("서울");
 
         tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
         setTabSpec();
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+    }
+
+    @OnClick(R.id.text_toolbarTitle)
+    public void onToolbarTitle() {
+        tabLayout.setVisibility(View.GONE);
+        //tabLayout.setElevation();
+        FragmentManager ft = getSupportFragmentManager();
+        ft.beginTransaction()
+                .replace(R.id.tab_linear, new LocationFragment())
+                .commit();
     }
 
     private void setTabSpec() {
         TextView tabSpec = (TextView)getLayoutInflater().inflate(R.layout.tab_spec, tabHost.getTabWidget(), false);
-        tabSpec.setText("뮤지컬");
+        tabSpec.setText("뮤 지 컬");
         tabHost.addTab(tabHost.newTabSpec(TAB_MUSICAL).setIndicator(tabSpec), MusicalFragment.class, null);
 
         tabSpec = (TextView)getLayoutInflater().inflate(R.layout.tab_spec, tabHost.getTabWidget(), false);
-        tabSpec.setText("오페라");
+        tabSpec.setText("오 페 라");
         tabHost.addTab(tabHost.newTabSpec(TAB_OPERA).setIndicator(tabSpec), OperaFragment.class, null);
 
         tabSpec = (TextView)getLayoutInflater().inflate(R.layout.tab_spec, tabHost.getTabWidget(), false);
-        tabSpec.setText("콘서트");
+        tabSpec.setText("콘 서 트");
         tabHost.addTab(tabHost.newTabSpec(TAB_CONCERT).setIndicator(tabSpec), ConcertFragment.class, null);
 
         tabSpec = (TextView)getLayoutInflater().inflate(R.layout.tab_spec, tabHost.getTabWidget(), false);
-        tabSpec.setText("위시리스트");
+        tabSpec.setText("위 시 리 스 트");
         tabHost.addTab(tabHost.newTabSpec(TAB_WISHLIST).setIndicator(tabSpec), WishListFragment.class, null);
 
         tabSpec = (TextView)getLayoutInflater().inflate(R.layout.tab_spec, tabHost.getTabWidget(), false);
-        tabSpec.setText("MD추천");
+        tabSpec.setText("M D 추 천");
         tabHost.addTab(tabHost.newTabSpec(TAB_MD).setIndicator(tabSpec), MDRecFragment.class, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.menu_detail : {
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                break;
+            }
+            case R.id.menu_search : {
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                break;
+            }
+        }
+
+        return true;
     }
 }
