52b80aad3bbdc580db1afd187f9ec1fe7c8566d3
Lim Jisu <lim_js91@naver.com>
2016-08-23 13:09:12 +0900

    Wang

---
+++ app/src/main/java/com/tacademy/singleplay/BookingPersonInfoActivity.java
@@ -0,0 +1,13 @@
+package com.tacademy.singleplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingPersonInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_person_info);
+    }
+}
---
+++ app/src/main/java/com/tacademy/singleplay/BookingSeatInfoActivity.java
@@ -0,0 +1,13 @@
+package com.tacademy.singleplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingSeatInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_seat_info);
+    }
+}
